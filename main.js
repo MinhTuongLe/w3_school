29d4abd6ed66017c28bc2f840950fe170b1cbff2
MinhTuongLe <leminhtuong09122002@gmail.com>
2023-02-27 18:08:27 +0700

    new commit

--- main.js
+++ main.js
@@ -42,39 +42,233 @@ window.onload = function () {
 // Dark mode
 var getChoiceItem = document.querySelector('.header__choices-item:first-child')
 var getIconDarkMode = document.querySelector('.search-side__icon:first-child')
-var getUserIcon = document.querySelector('.header__user-icon')
-var getDarkModeItem = document.querySelector('.dark-mode__item:first-child')
+var getUserIcons = document.querySelectorAll('.header__user-icon')
+var getDarkModeItem = document.querySelector('.dark-mode__item:first-child label')
 var getDarkCheckBox = document.querySelector('.dark-mode__item:first-child #dark-mode_checkbox')
+var getLessonListScroll = document.querySelector('.lesson-list-scroll')
+var getAdvertisementRightSide = document.querySelector('.advertisement__right-side')
+var getBody = document.querySelector('.body')
+var getContent = document.querySelector('.content')
+
+var getContentItem2 = document.querySelector('.content-item__2')
+var getContentItem7 = document.querySelector('.content-item__7')
+var getContentItem3Example = document.querySelector('.content-item__3-example')
+var getContentItem5Example = document.querySelector('.content-item__5-example')
+var getContentItem6Spans = document.querySelectorAll('.content-item__6 .content__line span')
+var getContentItem6As = document.querySelectorAll('.content-item__6-item .content__line a')
+var getContentItem9Exercises = document.querySelectorAll('.content-item__9-exercise')
+var getContentItem9ExerciseInputs = document.querySelectorAll('.content-item__9-exercise__input')
+var getContentItem9ExerciseContent = document.querySelector('.content-item__9-exercise__content')
+var getContentItem9ExerciseContentA = document.querySelector('.content-item__9-exercise__content a ')
+var getContentItem9ExerciseReds = document.querySelectorAll('.content-item__9-exercise--red')
+var getFooterBtn = document.querySelector('.footer__btn')
+var getFooterCourseList = document.querySelector('.footer__courses-list')
+var getFooterCourseAs = document.querySelectorAll('.footer__course a')
+var getFooterInformation = document.querySelector('.footer__information')
+var getFooterDescription = document.querySelector('.footer__description')
+var getFooterCopyRight = document.querySelector('.footer__copyright')
+var getFooterIcon = document.querySelector('.footer__icon')
+var getContentItem3 = document.querySelector('.content-item__3')
+var getContentItem3ExampleContent = document.querySelector('.content-item__3-example-content')
+var getFooterBtns = document.querySelector('.footer__btns')
+var getFooterCourseTitles = document.querySelectorAll('.footer__course-title')
+var getBorderBottoms = document.querySelectorAll('.border--bottom')
+var getBorderTops = document.querySelectorAll('.border--top')
+var getBorderRights = document.querySelectorAll('.border--right')
+
+
 
 var checkDarkMode = false;
-// getIconDarkMode.onclick = function onClick() {
-//     if (checkDarkMode == false) {
-//         getHeader.classList.add('dark_background')
-//         getChoiceItem.classList.add('.dark-mode_choice-item')
-//         getUserIcon.style.borderColor = "#ddd"
-//         checkDarkMode = true;
-//     } else {
-//         getHeader.classList.remove('dark_background')
-//         getChoiceItem.classList.remove('.dark-mode_choice-item')
-//         getUserIcon.style.borderColor = "#282A35"
-//         checkDarkMode = false;
-//     }
-// }
 
-getDarkModeItem.onclick = function () {
-    if (checkDarkMode == false || getDarkCheckBox.checked) {
+function showOrHideDarkMode() {
+    if (checkDarkMode == false) {
         getHeader.classList.add('dark_background')
         getChoiceItem.classList.add('.dark-mode_choice-item')
-        getUserIcon.style.borderColor = "#ddd"
-        checkDarkMode = true;
+        getUserIcons.forEach(function(item){
+            item.style.borderColor = "#ddd"
+        })
+        Object.assign(getLessonList.style, {
+            backgroundColor: "#15202b",
+            color: "#dddddd",
+        })
+        Object.assign(getAdvertisementRightSide.style, {
+            backgroundColor: "#1d2a35",
+            color: "#dddddd"
+        })
+
+        Object.assign(getContent.style, {
+            backgroundColor: "#1d2a35",
+            color:"#ddd"
+        })
+
+        getContentItem2.style.backgroundColor = "#193d3d"
+        getContentItem7.style.backgroundColor = "#193d3d"
+        getContentItem3Example.style.backgroundColor = "#38444d"
+
+        Object.assign(getContentItem5Example.style, {
+            backgroundColor:"#fff4a3",
+            color:"#1d2a35"
+        })
+        getContentItem6Spans.forEach(function(item){
+            item.style.color= "#ddd"
+        })
+        getContentItem6As.forEach(function(item){
+            item.style.color= "#ddd"
+        })
+        getContentItem9Exercises.forEach(function(item) {
+            item.style.backgroundColor = "#38444d"
+        })
+        getContentItem9ExerciseInputs.forEach(function(item) {
+            item.style.backgroundColor = "#38444d"
+        })
+
+        Object.assign(getContentItem9ExerciseContent.style, {
+            backgroundColor:"#15202b",
+            color:"#ddd"
+        })
+        Object.assign(getContentItem9ExerciseContentA.style, {
+            backgroundColor:"#15202b",
+            color:"#ddd"
+        })
+        getContentItem9ExerciseReds.forEach(function(item){
+            item.classList.add('content-item__9-exercise--red--dark-mode')
+        })
+
+
+        getFooterBtn.style.borderColor = "#ddd"
+        getFooterCourseList.style.backgroundColor = "#1d2a35"
+        getFooterInformation.style.backgroundColor = "#1d2a35"
+        getFooterDescription.style.backgroundColor = "#1d2a35"
+        getFooterCopyRight.style.backgroundColor = "#1d2a35"
+        getFooterIcon.style.backgroundColor = "#1d2a35"
+        getContentItem3.style.backgroundColor = "#1d2a35"
+        getContentItem3ExampleContent.style.backgroundColor = "#1d2a35"
+        getFooterBtns.style.backgroundColor = "#1d2a35"
+        getFooterCourseTitles.forEach(function(item) {
+            item.style.color = "#ddd"
+        })
+        getFooterCourseAs.forEach(function(item) {
+            Object.assign(item.style, {
+                backgroundColor:"#1d2a35",
+                color:"#dddddd"
+            })
+        })
+
+
+        getBorderBottoms.forEach(function(item){
+            item.style.borderColor = "#d9d9d941"
+        })
+        getBorderRights.forEach(function(item){
+            item.style.borderColor = "#d9d9d941"
+        })
+        getBorderTops.forEach(function(item){
+            item.style.borderColor = "#d9d9d941"
+        })
+
+
+        getLessonListScroll.classList.add('dark-mode__scroll-bar')
+        checkDarkMode = true
     } else {
         getHeader.classList.remove('dark_background')
         getChoiceItem.classList.remove('.dark-mode_choice-item')
-        getUserIcon.style.borderColor = "#282A35"
-        checkDarkMode = false;
+        getUserIcons.forEach(function(item){
+            item.style.borderColor = ""
+        })
+        Object.assign(getLessonList.style, {
+            backgroundColor: "",
+            color: ""
+        })
+        Object.assign(getAdvertisementRightSide.style, {
+            backgroundColor: "",
+            color: ""
+        })
+
+
+        
+        Object.assign(getContent.style, {
+            backgroundColor: "",
+            color:""
+        })
+
+        getContentItem2.style.backgroundColor = ""
+        getContentItem7.style.backgroundColor = ""
+        getContentItem3Example.style.backgroundColor = ""
+
+        Object.assign(getContentItem5Example.style, {
+            backgroundColor:"",
+            color:""
+        })
+        getContentItem6Spans.forEach(function(item){
+            item.style.color= ""
+        })
+        getContentItem6As.forEach(function(item){
+            item.style.color= ""
+        })
+        getContentItem9Exercises.forEach(function(item) {
+            item.style.backgroundColor = ""
+        })
+        getContentItem9ExerciseInputs.forEach(function(item) {
+            item.style.backgroundColor = ""
+        })
+
+
+        Object.assign(getContentItem9ExerciseContent.style, {
+            backgroundColor:"",
+            color:""
+        })
+        Object.assign(getContentItem9ExerciseContentA.style, {
+            backgroundColor:"",
+            color:""
+        })
+
+        getContentItem9ExerciseReds.forEach(function(item){
+            item.classList.remove('content-item__9-exercise--red--dark-mode')
+        })
+
+        getFooterBtn.style.borderColor = ""
+        getFooterCourseList.style.backgroundColor = ""
+        getFooterInformation.style.backgroundColor = ""
+        getFooterDescription.style.backgroundColor = ""
+        getFooterCopyRight.style.backgroundColor = ""
+        getFooterIcon.style.backgroundColor = ""
+        getContentItem3.style.backgroundColor = ""
+        getContentItem3ExampleContent.style.backgroundColor = ""
+        getFooterBtns.style.backgroundColor = ""
+        getFooterCourseTitles.forEach(function(item) {
+            item.style.color = ""
+        })
+
+        getFooterCourseAs.forEach(function(item) {
+            Object.assign(item.style, {
+                backgroundColor:"",
+                color:""
+            })
+        })
+
+        getBorderBottoms.forEach(function(item){
+            item.style.borderColor = ""
+        })
+        getBorderRights.forEach(function(item){
+            item.style.borderColor = ""
+        })
+        getBorderTops.forEach(function(item){
+            item.style.borderColor = ""
+        })
+
+
+
+
+        getLessonListScroll.classList.remove('dark-mode__scroll-bar')
+        checkDarkMode=false  
     }
 }
 
+getDarkModeItem.onclick = showOrHideDarkMode
+getIconDarkMode.onclick = showOrHideDarkMode
+
+
+
+// Search bar 
 
 var checkSearchBar = false
 var getSearchIcon = document.querySelector('.search-side__icon:last-child')
@@ -201,7 +395,7 @@ function showSubMenu2() {
         } else {
             hideSubMenu2_()
         }
-        
+
     } else if (checkClickSubmenu1 == true || checkClickSubmenu3 == true) {
         hideSubMenu1_()
         hideSubMenu3_()
@@ -252,7 +446,7 @@ function showSubMenu3() {
         } else {
             hideSubMenu3_()
         }
-        
+
     } else if (checkClickSubmenu1 == true || checkClickSubmenu2 == true) {
         hideSubMenu1_()
         hideSubMenu2_()
